860097c3fcc0890d6673f864092078e52eef0687
Kushal <kushal@gmx.com>
2021-09-23 21:20:54 -0600

    add stories

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
-var maximum = 0;
 fetch("https://hacker-news.firebaseio.com/v0/maxitem.json")
     .then(function (response) { return response.json(); })
     .then(function (data) {
-    maximum = data;
+    printItem(data);
     console.info({ data: data });
-    fetch("https://hacker-news.firebaseio.com/v0/item/" + maximum + ".json")
+});
+fetch("https://hacker-news.firebaseio.com/v0/topstories.json")
+    .then(function (response) { return response.json(); })
+    .then(function (data) {
+    var _a;
+    console.info({ data: data });
+    for (var _i = 0, data_1 = data; _i < data_1.length; _i++) {
+        var item = data_1[_i];
+        console.info({ item: item });
+        printItem(item);
+        if (((_a = item === null || item === void 0 ? void 0 : item.kids) === null || _a === void 0 ? void 0 : _a.length) > 0) {
+            for (var _b = 0, _c = item.kids; _b < _c.length; _b++) {
+                var kid = _c[_b];
+                printItem(kid);
+            }
+        }
+    }
+});
+function printItem(id) {
+    fetch("https://hacker-news.firebaseio.com/v0/item/" + id + ".json")
         .then(function (response) { return response.json(); })
         .then(function (data) {
         console.info({ data: data });
@@ -24,12 +42,18 @@ fetch("https://hacker-news.firebaseio.com/v0/maxitem.json")
         app === null || app === void 0 ? void 0 : app.appendChild(p3);
         if (data.type === "comment") {
             var p4 = document.createElement("p");
-            p1.className = "comment";
-            p4.textContent = "type: " + decodeHtml(data.text);
+            p4.className = "comment";
+            p4.textContent = "Comment text: " + decodeHtml(data.text);
+            app === null || app === void 0 ? void 0 : app.appendChild(p4);
+        }
+        if (data.type === "story") {
+            var p4 = document.createElement("p");
+            p4.className = "story";
+            p4.innerHTML = "Story: <a href=\"" + data.url + "\">" + decodeHtml(data.title) + "</a>";
             app === null || app === void 0 ? void 0 : app.appendChild(p4);
         }
     });
-});
+}
 function decodeHtml(html) {
     var txt = document.createElement("textarea");
     txt.innerHTML = html;
--- main.ts
+++ main.ts
@@ -1,41 +1,64 @@
-let maximum = 0;
-
 fetch("https://hacker-news.firebaseio.com/v0/maxitem.json")
     .then(response => response.json())
     .then(data => {
-        maximum = data;
+        printItem(data);
         console.info({ data });
-        fetch(`https://hacker-news.firebaseio.com/v0/item/${maximum}.json`)
-            .then(response => response.json())
-            .then(data => {
-                console.info({ data });
-                // 1. Select the div element using the id property
-                const app = document.getElementById("app");
-                // 2. Create a new <p></p> element programmatically
-                const p1 = document.createElement("p");
-                // 3. Add the text content
-                p1.textContent = `Id: ${data.id}`;
-                // 4. Append the p element to the div element
-                app?.appendChild(p1);
-
-                const p2 = document.createElement("p");
-                p2.textContent = `By: ${data.by}`;
-                app?.appendChild(p2);
-
-                const p3 = document.createElement("p");
-                p3.textContent = `type: ${data.type}`;
-                app?.appendChild(p3);
-
-                if (data.type === "comment") {
-                    const p4 = document.createElement("p");
-                    p1.className = "comment";
-                    p4.textContent = `type: ${decodeHtml(data.text)}`;
-                    app?.appendChild(p4);
-                }
-            });
     });
 
-function decodeHtml(html: string) {
+fetch("https://hacker-news.firebaseio.com/v0/topstories.json")
+    .then(response => response.json())
+    .then(data => {
+        console.info({ data });
+        for (const item of data) {
+            console.info({ item });
+            printItem(item);
+            if (item?.kids?.length > 0) {
+                for (let kid of item.kids) {
+                    printItem(kid);
+                }
+            }
+        }
+    })
+
+function printItem(id: number): void {
+    fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
+        .then(response => response.json())
+        .then(data => {
+            console.info({ data });
+            // 1. Select the div element using the id property
+            const app = document.getElementById("app");
+            // 2. Create a new <p></p> element programmatically
+            const p1 = document.createElement("p");
+            // 3. Add the text content
+            p1.textContent = `Id: ${data.id}`;
+            // 4. Append the p element to the div element
+            app?.appendChild(p1);
+
+            const p2 = document.createElement("p");
+            p2.textContent = `By: ${data.by}`;
+            app?.appendChild(p2);
+
+            const p3 = document.createElement("p");
+            p3.textContent = `type: ${data.type}`;
+            app?.appendChild(p3);
+
+            if (data.type === "comment") {
+                const p4 = document.createElement("p");
+                p4.className = "comment";
+                p4.textContent = `Comment text: ${decodeHtml(data.text)}`;
+                app?.appendChild(p4);
+            }
+
+            if (data.type === "story") {
+                const p4 = document.createElement("p");
+                p4.className = "story";
+                p4.innerHTML = `Story: <a href="${data.url}">${decodeHtml(data.title)}</a>`;
+                app?.appendChild(p4);
+            }
+        });
+}
+
+function decodeHtml(html: string): string {
     var txt = document.createElement("textarea");
     txt.innerHTML = html;
     return txt.value;
